09ab24e68f04f2a0cc5f5448f726b992ea946c0d
Ahmedtijani Umar <ahmedtijaniumar@gmail.com>
2019-08-23 23:01:14 +0100

    added

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+//deletes the data from the database using id
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+$connect = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+if(isset($_POST["id"]))
+{
+ $query = "DELETE FROM suppliers WHERE id = '".$_POST["id"]."'";
+ if(mysqli_query($connect, $query))
+ {
+  echo 'Supplier Deleted';
+ }
+}
+?>
